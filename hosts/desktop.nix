83fab70afa9a03ad71ab4cc3865e0b641d27ff0f
hefker <nelsonjulioaviles@gmail.com>
2026-04-12 12:29:47 -0500

    move hardware-specific desktop configs to hosts

---
+++ hosts/desktop.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  consts,
+  ...
+}:
+let
+  hostName = "desktop";
+in
+{
+  system.stateVersion = "25.11"; # Did you read the comment?
+  networking.hostName = hostName;
+
+  custom = {
+    roles = {
+      workstation = { };
+
+      gaming = {
+        packages.enable = true;
+        hardware.enable = true;
+      };
+    };
+
+    services = { };
+  };
+}
