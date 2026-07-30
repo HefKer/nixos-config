b82bbc8e1998914cda7fb9b9ec30d1b953c5630c
hefker <nelsonjulioaviles@gmail.com>
2026-07-29 22:54:39 -0500

    update global & project gitignore

--- homes/modules/cli.nix
+++ homes/modules/cli.nix
@@ -1,30 +1,39 @@
 { consts, ... }:
 {
-  programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        name = "hefker";
-        email = "nelsonjulioaviles@gmail.com";
+  programs = {
+
+    fish.enable = false;
+    starship.enable = false;
+    atuin.enable = false;
+    bat.enable = false;
+
+    git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "hefker";
+          email = "nelsonjulioaviles@gmail.com";
+        };
+        init.defaultBranch = "main";
+        pull.rebase = false;
       };
-      init.defaultBranch = "main";
-      pull.rebase = false;
+      ignores = [
+        # Syncthing
+        ".stglobalignore"
+        ".stignore"
+        ".stfolder/"
+
+        # Reference
+        "reference/"
+
+        # Agent files
+        "CLAUDE.md"
+        "CONTEXT.md"
+        "/.claude"
+        "/docs"
+        "/.scratch"
+      ];
     };
-    ignores = [
-      ".stglobalignore"
-      "CLAUDE.md"
-      ".claude/"
-      "docs/"
-      "CONTEXT.md"
-      "/.scratch"
-      "inspo/"
-      ".stfolder/"
-    ];
+
   };
-
-
-  programs.fish.enable = false;
-  programs.starship.enable = false;
-  programs.atuin.enable = false;
-  programs.bat.enable = false;
 }
